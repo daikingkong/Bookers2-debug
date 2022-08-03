3b1ae47c1bdc4aab22c4d0e27b33b999b58c0aa9
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-08-03 06:11:48 +0000

    [Fix]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,9 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = @book.id
-    comment.save
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
     # referer(リファラ)は
     # 「リンク元のURL（直前に見ていたページのURL）は、これです」
     # な情報が書いてある
